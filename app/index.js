15dfa0b1b617670a0f45cffa2f081c46f92059bb
beardman <beardman@internetbs.net>
2021-07-26 17:52:50 +0300

    init

---
+++ app/index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hola!')
+})
+
+app.get('/run', async (req, res) => {
+    const result = {}
+    res.send(result)
+})
+
+const server = app.listen(4040, console.log(`TestingHost`))
+server.keepAliveTimeout = 65000 // Ensure all inactive connections are terminated by the ALB, by setting this a few seconds higher than the ALB idle timeout
+server.headersTimeout = 66000
