dd7d3a71a54c959e381dde03e266eeb6fa19927c
beardman <beardman@internetbs.net>
2022-03-25 19:09:47 +0200

    fix

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 "use strict"
 
 const express = require("express")
-const controllers = require("./controllers")
+// const controllers = require("./controllers")
 const bot = require("./bot")
 
-const app = express()
-controllers.init(app)
+const port = process.env.PORT || 80
 
-const server = app.listen(80, () => {
-    console.log(`TestingHost on 4040 port`)
+const app = express()
+// controllers.init(app)
+
+app.get("/", (req, res) => {
+    res.send("Hola!")
+})
+
+app.get("/run", async (req, res) => {
+    const result = {}
+    res.send(result)
+})
+
+const server = app.listen(port, () => {
+    console.log(`TestingHost on ${port} port`)
 })
 server.keepAliveTimeout = 65000 // Ensure all inactive connections are terminated by the ALB, by setting this a few seconds higher than the ALB idle timeout
 server.headersTimeout = 66000
