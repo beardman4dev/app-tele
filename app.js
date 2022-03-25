b46cad6b70800fdec7fb18e4bccf3c2cedb465a1
beardman <beardman@internetbs.net>
2022-03-25 19:36:14 +0200

    fix

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express()
 app.get("/", (req, res) => res.send("Hola!"))
 app.get("/run", (req, res) => res.json({ ololo: "ololo" }))
 
-const server = app.listen(port, () => console.log(`Started on ${port} port.`))
+const server = app.listen(port, () => console.log({ started: `Started on ${port} port.`, date: new Date() }))
 
 server.keepAliveTimeout = 65000 // Ensure all inactive connections are terminated by the ALB, by setting this a few seconds higher than the ALB idle timeout
 server.headersTimeout = 66000
